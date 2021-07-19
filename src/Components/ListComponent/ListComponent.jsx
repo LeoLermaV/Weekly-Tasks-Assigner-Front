e706f65bd31191e8fe2688866cc4f7d116e0c207
leolermav <LeoLermaV@gmail.com>
2021-07-19 16:06:07 +1200

    added axios and mockup data for server

--- src/Components/ListComponent/ListComponent.jsx
+++ src/Components/ListComponent/ListComponent.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import axios from "axios";
+
+
+const submit = () => {
+    axios.post("http://localhost:3001/createweeklyassigner", {
+            name : 'leo',
+            taskAssigned : 'abc',
+            position : 6
+           
+        }).then(()=> {
+            console.log('ok i guess')
+        });
+           
+        };
+    
+
 
 export default ({Names, Tasks}) => (
     <div>
         <p>{Names}</p>
         <p>{Tasks}</p>
+        <button onClick={submit}>SUBMITTOBACKEND</button>
 
     </div>
 )
